9194ecca6e6ec5a6fa1378ddb82a5fe1164475be
TamayoChristian <105449824+TamayoChristian@users.noreply.github.com>
2024-07-14 10:30:26 -0500

    Delete app directory

--- app/src/main/java/com/example/alertaapp/Comunicacion.java
+++
@@ -1,7 +0,0 @@
-package com.example.alertaapp;
-
-public interface Comunicacion {
-    //void toggleProgressBar(boolean status);
-    void lanzarActividad(Class<?> tipoActividad);
-    void showMessage(String msg);
-}
